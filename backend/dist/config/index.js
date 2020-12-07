20c2c72e2659c9e7c16101f3c29d1b400aa21a1a
Mayank <mayank@amsyt.com>
2020-12-07 12:09:29 +0800

    Initial Commit

--- backend/dist/config/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.THUMBNAIL_PATH = exports.VIDEO_UPLOAD_PATH = void 0;
-exports.VIDEO_UPLOAD_PATH = "uploads/videos";
-exports.THUMBNAIL_PATH = "uploads/thumbnails";
-//# sourceMappingURL=index.js.map
